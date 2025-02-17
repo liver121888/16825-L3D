9dab5bdaf4ced756bc3479db51e13628a2980c18
liver121888 <liver121888@gmail.com>
2025-02-17 12:28:40 -0500

    Update main.py

--- assignment2/liweiy_code_proj02/main.py
+++ assignment2/liweiy_code_proj02/main.py
@@ -54,6 +54,15 @@ def render_3d(data, isMesh, output_path, args, dist=1):
     my_images = (rend[:, ..., :3].cpu().numpy() * 255).clip(0, 255).astype(np.uint8)
     imageio.mimsave(output_path, list(my_images), duration=1000//15, loop=0)
 
+import warnings
+warnings.simplefilter(action='ignore', category=DeprecationWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+parser = argparse.ArgumentParser("main", add_help=False)
+# run all questions
+parser.add_argument("q", default=0, type=int)
+main_args = parser.parse_args()
+
 
 output_prefix = "output/"
 if not os.path.exists(output_prefix):
@@ -63,125 +72,130 @@ if not os.path.exists(output_prefix):
 # 16-825 Assignment 2: Single View to 3D
 
 ## 1. Exploring loss functions
-class Q1Args:
-    lr = 4e-4
-    max_iter = 100
-    # max_iter = 100000
-    n_points = 5000
-    w_chamfer = 1.0
-    w_smooth = 0.1
-    device = 'cuda'
 
-q1_args = Q1Args()
+if main_args.q == 0 or main_args.q == 1:
 
-r2n2_dataset = R2N2("train", dataset_location.SHAPENET_PATH, dataset_location.R2N2_PATH, dataset_location.SPLITS_PATH, return_voxels=True)
-feed = r2n2_dataset[0]
+    class Q1Args:
+        lr = 4e-4
+        max_iter = 100
+        # max_iter = 100000
+        n_points = 5000
+        w_chamfer = 1.0
+        w_smooth = 0.1
+        device = 'cuda'
 
-feed_cuda = {}
-for k in feed:
-    if torch.is_tensor(feed[k]):
-        feed_cuda[k] = feed[k].to(q1_args.device).float()
+    q1_args = Q1Args()
 
-### 1.1. Fitting a voxel grid (5 points)
-voxels_src = torch.rand(feed_cuda['voxels'].shape,requires_grad=True, device=q1_args.device)
-voxel_coords = feed_cuda['voxel_coords'].unsqueeze(0)
-voxels_tgt = feed_cuda['voxels']
-fit_voxel(voxels_src, voxels_tgt, q1_args)
+    r2n2_dataset = R2N2("train", dataset_location.SHAPENET_PATH, dataset_location.R2N2_PATH, dataset_location.SPLITS_PATH, return_voxels=True)
+    feed = r2n2_dataset[0]
 
-# vox to mesh
-mesh = pytorch3d.ops.cubify(voxels_tgt, device=voxels_tgt.device, thresh=0.5)
-vertices, faces = mesh.verts_list()[0], mesh.faces_list()[0]
-vertices = vertices.unsqueeze(0)
-faces = faces.unsqueeze(0)
-textures = torch.ones_like(vertices)
-textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
-print(vertices.shape, faces.shape, textures.shape)
-mesh = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures))
-render_3d(mesh, True, output_prefix + 'voxel_fit_tgt.gif', q1_args, dist=4.0)
+    feed_cuda = {}
+    for k in feed:
+        if torch.is_tensor(feed[k]):
+            feed_cuda[k] = feed[k].to(q1_args.device).float()
 
-# vox to mesh
-mesh = pytorch3d.ops.cubify(voxels_src, device=voxels_src.device, thresh=0.5)
-vertices, faces = mesh.verts_list()[0], mesh.faces_list()[0]
-vertices = vertices.unsqueeze(0)
-faces = faces.unsqueeze(0)
-textures = torch.ones_like(vertices)
-textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
-print(vertices.shape, faces.shape, textures.shape)
-mesh = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures))
-render_3d(mesh, True, output_prefix + 'voxel_fit_src.gif', q1_args, dist=4.0)
+    ### 1.1. Fitting a voxel grid (5 points)
+    voxels_src = torch.rand(feed_cuda['voxels'].shape,requires_grad=True, device=q1_args.device)
+    voxel_coords = feed_cuda['voxel_coords'].unsqueeze(0)
+    voxels_tgt = feed_cuda['voxels']
+    fit_voxel(voxels_src, voxels_tgt, q1_args)
 
+    # vox to mesh
+    mesh = pytorch3d.ops.cubify(voxels_tgt, device=voxels_tgt.device, thresh=0.5)
+    vertices, faces = mesh.verts_list()[0], mesh.faces_list()[0]
+    vertices = vertices.unsqueeze(0)
+    faces = faces.unsqueeze(0)
+    textures = torch.ones_like(vertices)
+    textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
+    print(vertices.shape, faces.shape, textures.shape)
+    mesh = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures))
+    render_3d(mesh, True, output_prefix + 'voxel_fit_tgt.gif', q1_args, dist=4.0)
 
-### 1.2. Fitting a point cloud (5 points)
-pointclouds_src = torch.randn([1,q1_args.n_points,3],requires_grad=True, device=q1_args.device)
-mesh_tgt = pytorch3d.structures.Meshes(verts=[feed_cuda['verts']], faces=[feed_cuda['faces']])
-pointclouds_tgt = sample_points_from_meshes(mesh_tgt, q1_args.n_points)
-fit_pointcloud(pointclouds_src, pointclouds_tgt, q1_args)
-
-r = torch.ones(pointclouds_src.shape[-2:])
-r = (r * torch.tensor([0.7, 0.7, 1])).unsqueeze(0).to(q1_args.device)
-print(pointclouds_src.shape, r.shape)
-point_cloud_tgt = pytorch3d.structures.Pointclouds(points=pointclouds_tgt, features=r).detach()
-point_cloud = pytorch3d.structures.Pointclouds(points=pointclouds_src, features=r).detach()
-render_3d(point_cloud_tgt, False, output_prefix + 'point_cloud_fit_tgt.gif', q1_args, dist=2.0)
-render_3d(point_cloud, False, output_prefix + 'point_cloud_fit.gif', q1_args, dist=2.0)
-
-### 1.3. Fitting a mesh (5 points)
-mesh_src = ico_sphere(4, q1_args.device)
-mesh_tgt = pytorch3d.structures.Meshes(verts=[feed_cuda['verts']], faces=[feed_cuda['faces']])
-
-# fitting
-fit_mesh(mesh_src, mesh_tgt, q1_args)    
+    # vox to mesh
+    mesh = pytorch3d.ops.cubify(voxels_src, device=voxels_src.device, thresh=0.5)
+    vertices, faces = mesh.verts_list()[0], mesh.faces_list()[0]
+    vertices = vertices.unsqueeze(0)
+    faces = faces.unsqueeze(0)
+    textures = torch.ones_like(vertices)
+    textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
+    print(vertices.shape, faces.shape, textures.shape)
+    mesh = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures))
+    render_3d(mesh, True, output_prefix + 'voxel_fit_src.gif', q1_args, dist=4.0)
 
 
-vertices, faces = mesh_tgt.verts_list()[0], mesh_tgt.faces_list()[0]
-vertices = vertices.unsqueeze(0)
-faces = faces.unsqueeze(0)
-textures = torch.ones_like(vertices)
-textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
-# print(vertices.shape, faces.shape, textures.shape)
-mesh_tgt = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures)).detach()
+    ### 1.2. Fitting a point cloud (5 points)
+    pointclouds_src = torch.randn([1,q1_args.n_points,3],requires_grad=True, device=q1_args.device)
+    mesh_tgt = pytorch3d.structures.Meshes(verts=[feed_cuda['verts']], faces=[feed_cuda['faces']])
+    pointclouds_tgt = sample_points_from_meshes(mesh_tgt, q1_args.n_points)
+    fit_pointcloud(pointclouds_src, pointclouds_tgt, q1_args)
 
-vertices, faces = mesh_src.verts_list()[0], mesh_src.faces_list()[0]
-vertices = vertices.unsqueeze(0)
-faces = faces.unsqueeze(0)
-textures = torch.ones_like(vertices)
-textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
-# print(vertices.shape, faces.shape, textures.shape)
-mesh_src = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures)).detach()
+    r = torch.ones(pointclouds_src.shape[-2:])
+    r = (r * torch.tensor([0.7, 0.7, 1])).unsqueeze(0).to(q1_args.device)
+    print(pointclouds_src.shape, r.shape)
+    point_cloud_tgt = pytorch3d.structures.Pointclouds(points=pointclouds_tgt, features=r).detach()
+    point_cloud = pytorch3d.structures.Pointclouds(points=pointclouds_src, features=r).detach()
+    render_3d(point_cloud_tgt, False, output_prefix + 'point_cloud_fit_tgt.gif', q1_args, dist=2.0)
+    render_3d(point_cloud, False, output_prefix + 'point_cloud_fit.gif', q1_args, dist=2.0)
 
-render_3d(mesh_tgt, True, output_prefix + 'mesh_tgt.gif', q1_args, dist=1.2)
-render_3d(mesh_src, True, output_prefix + 'mesh_fit.gif', q1_args, dist=1.2)
+    ### 1.3. Fitting a mesh (5 points)
+    mesh_src = ico_sphere(4, q1_args.device)
+    mesh_tgt = pytorch3d.structures.Meshes(verts=[feed_cuda['verts']], faces=[feed_cuda['faces']])
+
+    # fitting
+    fit_mesh(mesh_src, mesh_tgt, q1_args)    
+
+
+    vertices, faces = mesh_tgt.verts_list()[0], mesh_tgt.faces_list()[0]
+    vertices = vertices.unsqueeze(0)
+    faces = faces.unsqueeze(0)
+    textures = torch.ones_like(vertices)
+    textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
+    # print(vertices.shape, faces.shape, textures.shape)
+    mesh_tgt = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures)).detach()
+
+    vertices, faces = mesh_src.verts_list()[0], mesh_src.faces_list()[0]
+    vertices = vertices.unsqueeze(0)
+    faces = faces.unsqueeze(0)
+    textures = torch.ones_like(vertices)
+    textures = textures * torch.tensor([0.7, 0.7, 1]).to(q1_args.device)
+    # print(vertices.shape, faces.shape, textures.shape)
+    mesh_src = pytorch3d.structures.Meshes(verts=vertices, faces=faces, textures=pytorch3d.renderer.TexturesVertex(textures)).detach()
+
+    render_3d(mesh_tgt, True, output_prefix + 'mesh_tgt.gif', q1_args, dist=1.2)
+    render_3d(mesh_src, True, output_prefix + 'mesh_fit.gif', q1_args, dist=1.2)
 
 
 ## 2. Reconstructing 3D from single view
 
-class Q2Args:
-    arch="resnet18"
-    lr=4e-4
-    # max_iter=100
-    max_iter=100000
-    batch_size=32
-    num_workers=4
-    n_points=1000
-    w_chamfer=1.0
-    w_smooth=0.1
-    save_freq=2000
-    # save_freq=20
-    load_checkpoint=True
-    device="cuda"
-    load_feat=True
-    # ["vox", "point", "mesh"]
-    type="vox"
+if main_args.q == 0 or main_args.q == 2:
 
-q2_args = Q2Args()
+    class Q2Args:
+        arch="resnet18"
+        lr=4e-4
+        max_iter=100
+        # max_iter=100000
+        batch_size=32
+        num_workers=4
+        n_points=1000
+        w_chamfer=1.0
+        w_smooth=0.1
+        # save_freq=2000
+        save_freq=20
+        load_checkpoint=False
+        device="cuda"
+        load_feat=True
+        # ["vox", "point", "mesh"]
+        type="vox"
 
-### 2.1. Image to voxel grid (20 points)
-train_model(q2_args)
+    q2_args = Q2Args()
 
-### 2.2. Image to point cloud (20 points)
-q2_args.type = "point"
-train_model(q2_args)
+    ### 2.1. Image to voxel grid (20 points)
+    train_model(q2_args)
 
-### 2.3. Image to mesh (20 points)
-q2_args.type = "mesh"
-train_model(q2_args)
+    ### 2.2. Image to point cloud (20 points)
+    q2_args.type = "point"
+    train_model(q2_args)
+
+    ### 2.3. Image to mesh (20 points)
+    q2_args.type = "mesh"
+    train_model(q2_args)
